447052640fdad5a162cd8514f8f4b48364b60287
Keyur Patel <keyurpatel_kc@yahoo.com>
2017-02-16 20:20:18 -0800

    Update Constants.java

--- app/src/main/java/hw1/android/csulb/edu/hw1/Extras/Constants.java
+++ app/src/main/java/hw1/android/csulb/edu/hw1/Extras/Constants.java
@@ -5,7 +5,7 @@ package hw1.android.csulb.edu.hw1.Extras;
  */
 
 public final class Constants {
-    public final static String COUNTRY_CODE = "+1";
-    public final static String RESTAURANT_NUMBER = "5626072065";
-    public final static String XML_FILEPATH = "XML/item_list.xml";
+    public final static String COUNTRY_CODE = "+1"; //Select the country code for the contact number.
+    public final static String RESTAURANT_NUMBER = ""; // Add the contact number of the Taco Restaurant here.
+    public final static String XML_FILEPATH = "XML/item_list.xml"; // Menu is located at this path.
 }
